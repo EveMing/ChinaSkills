f75a822cf52a97fd63ebf67897a98c24f59c1f00
BigBug <1373396036@wq.com>
2019-04-28 14:09:54 +0800

    LED写屏

--- WlW/WPF/LED_Print/LED_Print/MainWindow.xaml.cs
+++ WlW/WPF/LED_Print/LED_Print/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//
+using System.IO.Ports;
+using LEDLibrary;
+
 
 namespace LED_Print
 {
@@ -23,6 +27,30 @@ namespace LED_Print
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private LEDPlayer player;
+
+        private void ComList1_Loaded(object sender, RoutedEventArgs e)
+        {
+            //获取串口号
+            foreach (var item in SerialPort.GetPortNames()) {
+
+                this.ComList1.Items.Add(item);
+            }
+        }
+
+        private void Btn_start1_Click(object sender, RoutedEventArgs e)
+        {
+            if (ComList1.SelectedValue == null) {
+
+                MessageBox.Show("请选择串口");
+                return;
+            }
+            //发送LED字符串
+            player = new LEDPlayer(this.ComList1.SelectedItem.ToString());
+            MessageBox.Show(player.DisplayText(this.txt_input1.Text));
+            
         }
     }
 }
