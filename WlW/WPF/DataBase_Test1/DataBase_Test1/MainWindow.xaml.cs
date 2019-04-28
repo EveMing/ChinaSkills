f739325ed60a14a7b83f9c5fd4a0075007d1732d
BigBug <1373396036@wq.com>
2019-04-28 11:33:30 +0800

    select,insert,delete

--- WlW/WPF/DataBase_Test1/DataBase_Test1/MainWindow.xaml.cs
+++ WlW/WPF/DataBase_Test1/DataBase_Test1/MainWindow.xaml.cs
@@ -28,45 +28,61 @@ namespace DataBase_Test1
         {
             InitializeComponent();
         }
-        public Dictionary<int, string> dicDB;
+
 
 
         //查询 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+
+            this.listBox.Items.Clear();
             dbConnect(0, "SELECT name from users");
             //foreach(var data in)
         }
 
 
-        public Dictionary<int, string> dbConnect(int Type, string str)
+        
+
+        private void btn_insert_Click(object sender, RoutedEventArgs e)
+        {
+            dbConnect(1, "");
+        }
+
+        private void Btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            
+            dbConnect(2, "delete from users where name=@name");
+            this.listBox.Items.Clear();
+            dbConnect(0, "SELECT name from users");
+        }
+
+
+        public void dbConnect(int Type, string str)
         {
 
-            string strConn = "data source=127.0.0.1;initial catalog=Test;user id=sa;password=123456;";
+            string strConn = "data source=192.168.100.40;initial catalog=test;user id=sa;password=qwer1234.;";
             using (SqlConnection conn = new SqlConnection(strConn))
             {
                 try
                 {
                     conn.Open();
-                    
 
-                    switch (Type) {
+
+                    switch (Type)
+                    {
 
                         //查询
-                        case 0 :
+                        case 0:
 
                             SqlCommand cmd = new SqlCommand(str, conn);
-                            //SqlDataAdapter sqldata = new SqlDataAdapter(cmd);
 
+                            //执行返回第一行第一列
                             //object sele = cmd.ExecuteScalar();
-
-                            //conn.Close();
-
-                            dicDB = new Dictionary<int, string>();
 
                             SqlDataReader re = cmd.ExecuteReader();
 
-                            while (re.Read()) {
+                            while (re.Read())
+                            {
 
                                 this.listBox.Items.Add(re["name"]);
 
@@ -74,33 +90,47 @@ namespace DataBase_Test1
 
                             conn.Close();
 
-                            return dicDB;
+                            return;
 
- 
+
                         //添加     
                         case 1:
-                            using (SqlCommand comm=conn.CreateCommand()) {
+                            using (SqlCommand comm = conn.CreateCommand())
+                            {
                                 string sqlStr = "insert into users(name) values(@name)";
                                 comm.CommandText = sqlStr;
-                                comm.Parameters.AddWithValue("@name",this.txt_insert.Text);
+                                comm.Parameters.AddWithValue("@name", this.txt_insert.Text);
                                 if (comm.ExecuteNonQuery() > 0)
                                 {
                                     MessageBox.Show("插入成功");
-
+                                    this.txt_insert.Text = null;
                                 }
-                                else {
+                                else
+                                {
                                     MessageBox.Show("插入失败");
 
                                 };
 
                             }
-                                break;
+                            conn.Close();
+                            break;
 
 
 
                         //删除
                         case 2:
+                            using (SqlCommand com = conn.CreateCommand())
+                            {
+                                //string sql = "delete from users where name=@name";
+                                com.CommandText = str;
+                                com.Parameters.AddWithValue("@name", this.listBox.SelectedItem.ToString());
+                                if (com.ExecuteNonQuery() > 0)
+                                {
 
+                                    MessageBox.Show("删除成功");
+                                }
+
+                            }
                             break;
 
 
@@ -112,14 +142,9 @@ namespace DataBase_Test1
                     Console.Write(s);
 
                 }
-                return dicDB;
+                return;
 
             }
-        }
-
-        private void btn_insert_Click(object sender, RoutedEventArgs e)
-        {
-            dbConnect(1, "");
         }
     }
 
