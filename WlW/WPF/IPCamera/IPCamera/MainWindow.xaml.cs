1374efdd5c1774d38eb242ec68938997ff3eeecc
BigBug <1373396036@wq.com>
2019-04-28 21:53:02 +0800

    Model

---
+++ WlW/WPF/IPCamera/IPCamera/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//
+using Camera.Net;
+using IPCamera.Properties;
+
+
+
+
+
+namespace IPCamera
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        CameraCapture camera;
+        private void PanleShow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Comlist.Items.Add("rtsp");
+            this.Comlist.Items.Add("http");
+
+        }
+        private void Btn_Open_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (this.Comlist.SelectedItem.ToString() == "rtsp")
+            {
+                string connStr = this.Comlist.SelectedItem.ToString() + "://" + this.txt_user.Text + ":" + this.txt_Passw.Text + "@" + this.txt_IP.Text + "/11";
+                camera.Open(connStr);
+
+                return;
+            }
+            else
+            {
+                MessageBox.Show("http");
+                string connStr = this.Comlist.SelectedItem.ToString() + "://" + this.txt_IP.Text + ":80/videostream.cgi?loginuse=" + this.txt_user.Text + "&loginpas=" + this.txt_Passw.Text;
+                camera.Open(connStr);
+
+            }
+
+            
+
+
+        }
+
+
+    }
+}
