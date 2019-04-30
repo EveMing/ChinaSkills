1a38e0fcc84f508d39236ad6ef621e33da5bef11
BigBug <1373396036@qq.com>
2019-04-30 20:20:03 +0800

    Code 1

--- WlW/WPF/ADAM4150/ADAM4150/MainWindow.xaml.cs
+++ WlW/WPF/ADAM4150/ADAM4150/MainWindow.xaml.cs
@@ -12,9 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//
 using System.IO.Ports;
 using DigitalLibrary;
-using ComLibrary;
+using System.Windows.Threading;
 
 namespace ADAM4150
 {
@@ -33,10 +34,6 @@ namespace ADAM4150
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             getPort();
-
-
-
-
         }
 
         public void getPort()
@@ -44,16 +41,12 @@ namespace ADAM4150
             //读取串口
             foreach (var item in SerialPort.GetPortNames())
             {
-
                 this.Comlist.Items.Add(item);
             }
-
-
-
         }
-
-        private void Btn_start_Click(object sender, RoutedEventArgs e)
+        public void getValue(object sender, EventArgs e)
         {
+            //Console.Write("log:"+e.ToString());
             if (this.Comlist.SelectedIndex < 0)
             {
                 MessageBox.Show("请选择串口");
@@ -68,61 +61,99 @@ namespace ADAM4150
 
             adam.SetData();
 
-            lab_fire.Content = adam.DI5;
-            lab_humanbody.Content = adam.DI2;
-            lab_infrared.Content = adam.DI4;
-            lab_smoke.Content = adam.DI3;
-            
-            
+            lab_fire.Content = adam.DI1 ? "有火灾" : "一切正常";
+            lab_humanbody.Content = adam.DI0 ? "有人" : "一切正常";
+            lab_infrared.Content = adam.DI4 ? "有人入侵" : "一切正常";
+            lab_smoke.Content = adam.DI2 ? "有烟雾" : "一切正常";
 
         }
 
+        private void Btn_start_Click(object sender, RoutedEventArgs e)
+        {
+
+            DispatcherTimer dis = new DispatcherTimer();
+            dis.Tick += new EventHandler(getValue);
+            dis.Interval = TimeSpan.FromMilliseconds(1000);
+            dis.Start();
+
+
+        }
+
+
+
         public bool ExecuteOnOff(ADAM4150FuncID ID)
         {
-            
-
+            if (this.Comlist.SelectedIndex < 0)
+            {
+                MessageBox.Show("请选择串口");
+                return false;
+            }
             comset = new ComSettingModel();
 
             comset.DigitalQuantityCom = Comlist.SelectedValue.ToString();
 
             adam = new DigitalLibrary.ADAM4150(comset);
 
-
             return adam.OnOff(ID);
 
         }
 
+        //灯泡
         private void Btn_lamp_Click(object sender, RoutedEventArgs e)
         {
-            if (ExecuteOnOff(ADAM4150FuncID.OnDO1))
+
+            if (this.btn_lamp.Content.ToString() == "打开" && ExecuteOnOff(ADAM4150FuncID.OnDO1))
             {
 
-                MessageBox.Show("打开成功");
+                this.btn_lamp.Content = "关闭";
+                return;
+            }
+            else if (this.btn_lamp.Content.ToString() != "打开")
+            {
+                ExecuteOnOff(ADAM4150FuncID.OffDO1);
+                this.btn_lamp.Content = "打开";
                 return;
             }
             MessageBox.Show("打开失败");
         }
 
+        //报警灯
         private void Btn_lamp_police_Click(object sender, RoutedEventArgs e)
         {
-            if (ExecuteOnOff(ADAM4150FuncID.OnDO0))
+            if (this.btn_lamp_police.Content.ToString() == "打开" && ExecuteOnOff(ADAM4150FuncID.OnDO0))
             {
 
-                MessageBox.Show("打开成功");
+
+                this.btn_lamp_police.Content = "关闭";
+                return;
+            }
+            else if (this.btn_lamp_police.Content.ToString() != "打开")
+            {
+                ExecuteOnOff(ADAM4150FuncID.OffDO0);
+                this.btn_lamp_police.Content = "打开";
                 return;
             }
             MessageBox.Show("打开失败");
+
         }
 
+        //风扇
         private void Btn_wind_Click(object sender, RoutedEventArgs e)
         {
-            if (ExecuteOnOff(ADAM4150FuncID.OnDO2))
+            if (this.btn_wind.Content.ToString() == "打开" && ExecuteOnOff(ADAM4150FuncID.OnDO2))
             {
 
-                MessageBox.Show("打开成功");
+                this.btn_wind.Content = "关闭";
+                return;
+            }
+            if (this.btn_wind.Content.ToString() != "打开")
+            {
+                ExecuteOnOff(ADAM4150FuncID.OffDO2);
+                this.btn_wind.Content = "打开";
                 return;
             }
             MessageBox.Show("打开失败");
+
         }
     }
 }
