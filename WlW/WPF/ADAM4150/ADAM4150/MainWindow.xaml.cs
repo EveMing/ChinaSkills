53689a4b8416fb03e8cd63ac84ded7542a906b6d
BigBug <1373396036@wq.com>
2019-04-30 18:29:25 +0800

    first

--- WlW/WPF/ADAM4150/ADAM4150/MainWindow.xaml.cs
+++ WlW/WPF/ADAM4150/ADAM4150/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
+using DigitalLibrary;
+using ComLibrary;
 
 namespace ADAM4150
 {
@@ -26,14 +28,19 @@ namespace ADAM4150
             InitializeComponent();
         }
 
+        public ComSettingModel comset;
+        public DigitalLibrary.ADAM4150 adam;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             getPort();
 
 
+
+
         }
 
-        public void getPort() {
+        public void getPort()
+        {
             //读取串口
             foreach (var item in SerialPort.GetPortNames())
             {
@@ -41,13 +48,81 @@ namespace ADAM4150
                 this.Comlist.Items.Add(item);
             }
 
-            if (this.Comlist.SelectedIndex>0) {
 
-                MessageBox.Show("请选择串口");
-
-            }
 
         }
 
+        private void Btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Comlist.SelectedIndex < 0)
+            {
+                MessageBox.Show("请选择串口");
+                return;
+            }
+
+            comset = new ComSettingModel();
+
+            comset.DigitalQuantityCom = Comlist.SelectedValue.ToString();
+
+            adam = new DigitalLibrary.ADAM4150(comset);
+
+            adam.SetData();
+
+            lab_fire.Content = adam.DI5;
+            lab_humanbody.Content = adam.DI2;
+            lab_infrared.Content = adam.DI4;
+            lab_smoke.Content = adam.DI3;
+            
+            
+
+        }
+
+        public bool ExecuteOnOff(ADAM4150FuncID ID)
+        {
+            
+
+            comset = new ComSettingModel();
+
+            comset.DigitalQuantityCom = Comlist.SelectedValue.ToString();
+
+            adam = new DigitalLibrary.ADAM4150(comset);
+
+
+            return adam.OnOff(ID);
+
+        }
+
+        private void Btn_lamp_Click(object sender, RoutedEventArgs e)
+        {
+            if (ExecuteOnOff(ADAM4150FuncID.OnDO1))
+            {
+
+                MessageBox.Show("打开成功");
+                return;
+            }
+            MessageBox.Show("打开失败");
+        }
+
+        private void Btn_lamp_police_Click(object sender, RoutedEventArgs e)
+        {
+            if (ExecuteOnOff(ADAM4150FuncID.OnDO0))
+            {
+
+                MessageBox.Show("打开成功");
+                return;
+            }
+            MessageBox.Show("打开失败");
+        }
+
+        private void Btn_wind_Click(object sender, RoutedEventArgs e)
+        {
+            if (ExecuteOnOff(ADAM4150FuncID.OnDO2))
+            {
+
+                MessageBox.Show("打开成功");
+                return;
+            }
+            MessageBox.Show("打开失败");
+        }
     }
 }
